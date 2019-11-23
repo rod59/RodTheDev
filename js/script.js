4eb9567d085d7dc60b17a8b3d193ddaf34b6c493
rod <rod_rahmjoo@yahoo.com>
2019-11-22 21:29:42 -0500

    removed slow ass jqery; replaced with incomplete javascript. removed css and added scss. MUCH easier to read

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+window.onscroll = function (e) {
+	var barLight = document.getElementsByClassName('proficiency-bar-navy');
+	var barDark = document.getElementsByClassName('proficiency-bar-black');
+	var elemTop = document.getElementsByClassName('skills');
+	console.log(elemTop.offsetTop);
+	if(window.scrollY > elemTop.offsetTop){
+		for(var i = 0; i<barLight.length; i++){
+			barLight[i].style.display = 'block';
+		}
+		for(var i = 0; i<barDark.length; i++){
+			barDark[i].style.display = 'block';
+		}
+	}
+	else{
+		for(var i = 0; i<barLight.length; i++){
+			barLight[i].style.display = 'none';
+		}
+		for(var i = 0; i<barDark.length; i++){
+			barDark[i].style.display = 'none';
+		}
+	}
+}
+
+
 function ul(index) {
 	console.log('click!' + index)
 
