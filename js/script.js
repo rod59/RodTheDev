156feffb2f883d1359346d05021de1de25569920
rod <rod_rahmjoo@yahoo.com>
2019-11-23 17:55:06 -0500

    fixed bars sliding in out. officially jquery free zone

--- js/script.js
+++ js/script.js
@@ -1,27 +1,33 @@
+/*BARS SLIDING IN OUT FUNCTIONALITY*/
 window.onscroll = function (e) {
-	var barLight = document.getElementsByClassName('proficiency-bar-navy');
-	var barDark = document.getElementsByClassName('proficiency-bar-black');
+	var bar = document.getElementsByClassName('proficiency-bar');
 	var elemTop = document.getElementsByClassName('skills');
-	console.log(elemTop.offsetTop);
-	if(window.scrollY > elemTop.offsetTop){
-		for(var i = 0; i<barLight.length; i++){
-			barLight[i].style.display = 'block';
-		}
-		for(var i = 0; i<barDark.length; i++){
-			barDark[i].style.display = 'block';
+	var changeFactor = 1000;
+	if(window.scrollY > elemTop[0].offsetTop/1.5){
+		if(bar[0].style.width.slice(0, -1)<1){
+				for(var i = 0; i<bar.length; i++){
+					var originalWidth = bar[i].style.width.slice(0, -1) * changeFactor;
+					for(var j = 0; j < originalWidth; j++ ){
+						bar[i].style.width = j + "%";
+				}
+				bar[i].style.width = originalWidth + "%";
+			}
 		}
 	}
 	else{
-		for(var i = 0; i<barLight.length; i++){
-			barLight[i].style.display = 'none';
-		}
-		for(var i = 0; i<barDark.length; i++){
-			barDark[i].style.display = 'none';
+		if(bar[0].style.width.slice(0, -1)>1){
+			for(var i = 0; i<bar.length; i++){
+				var originalWidth	= bar[i].style.width.slice(0, -1);
+				for(var j = originalWidth; j > 0; j-- ){
+					bar[i].style.width = j + "%";
+				}
+				bar[i].style.width	= originalWidth/changeFactor + "%";
+			}
 		}
 	}
 }
 
-
+/*NAV FUNCTIONALITY*/
 function ul(index) {
 	console.log('click!' + index)
 
