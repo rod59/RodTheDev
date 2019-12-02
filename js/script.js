fe8682b3e1626e814abfe11b28daf187ff7fa6a9
rod <rod_rahmjoo@yahoo.com>
2019-12-02 00:48:37 -0500

    made the top cube a lot nicer and removed nav bar to add different kind of nav

--- js/script.js
+++ js/script.js
@@ -72,14 +72,3 @@ window.onscroll = function (e) {
 	}, 1);
 	}
 biggestMoveLeft();
-
-/*NAV FUNCTIONALITY*/
-function ul(index) {
-	console.log('click!' + index)
-
-	var underlines = document.querySelectorAll(".underline");
-
-	for (var i = 0; i < underlines.length; i++) {
-		underlines[i].style.transform = 'translate3d(' + index * 100 + '%,0,0)';
-	}
-}
