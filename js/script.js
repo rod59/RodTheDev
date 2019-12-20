2fdd2745494827189a803313db2746d73919dd33
rod <rod_rahmjoo@yahoo.com>
2019-12-20 08:09:16 -0500

    BIG UPDATE, entire nav changed and entire top portionn of the site

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
 /*BARS SLIDING IN OUT FUNCTIONALITY*/
-var bar = document.getElementsByClassName('proficiency-bar');
-var changeFactor = 1000;
+let bar = document.getElementsByClassName('proficiency-bar');
+let changeFactor = 1000;
 
 window.onload = function() {
-  for (var i = 0; i < bar.length; i++) {
+  for (let i = 0; i < bar.length; i++) {
     bar[i].style.transitionDuration = '0s';
-    var originalWidth = bar[i].style.width.slice(0, -1);
+    let originalWidth = bar[i].style.width.slice(0, -1);
     bar[i].style.width = originalWidth / changeFactor + "%";
   }
 }
 window.onscroll = function(e) {
-  var elemTop = document.getElementsByClassName('skills');
+  let elemTop = document.getElementsByClassName('skills');
   if (window.scrollY > elemTop[0].offsetTop / 1.5) {
     if (bar[0].style.width.slice(0, -1) < 1) {
-      for (var i = 0; i < bar.length; i++) {
+      for (let i = 0; i < bar.length; i++) {
         bar[i].style.transitionDuration = '1s';
-        var originalWidth = bar[i].style.width.slice(0, -1) * changeFactor;
+        let originalWidth = bar[i].style.width.slice(0, -1) * changeFactor;
         bar[i].style.width = originalWidth + "%";
       }
     }
   } else {
     if (bar[0].style.width.slice(0, -1) > 1) {
-      for (var i = 0; i < bar.length; i++) {
+      for (let i = 0; i < bar.length; i++) {
         bar[i].style.transitionDuration = '1s';
-        var originalWidth = bar[i].style.width.slice(0, -1);
+        let originalWidth = bar[i].style.width.slice(0, -1);
         bar[i].style.width = originalWidth / changeFactor + "%";
       }
     }
@@ -34,8 +34,8 @@ window.onscroll = function(e) {
 //THE ORDER OF THESE ARRAYS MATTERS!!!
 //EACH POSITION IS LINKED (ex: currentCloudMargin[0] associated with currentCloud[0])
 //SPEED OF MOVEMENT IS LINKED TO POSITIONING IN ARRAY AS WELL
-var currentCloudMargin = [400, 800, 100, 600, 200, 1000, 300, 1200];
-var currentCloud = [document.getElementsByClassName('small-cloud')[0],
+let currentCloudMargin = [400, 800, 100, 600, 200, 1000, 300, 1200];
+let currentCloud = [document.getElementsByClassName('small-cloud')[0],
   document.getElementsByClassName('small-cloud')[1],
   document.getElementsByClassName('mid-cloud')[0],
   document.getElementsByClassName('mid-cloud')[1],
@@ -45,8 +45,8 @@ var currentCloud = [document.getElementsByClassName('small-cloud')[0],
   document.getElementsByClassName('biggest-cloud')[1]
 ];
 
-var biggestMoveLeft = (function() {
-  for (var i = 0; i < currentCloud.length; i++) {
+let biggestMoveLeft = (function() {
+  for (let i = 0; i < currentCloud.length; i++) {
     continueCloud(i);
   }
 });
@@ -58,8 +58,8 @@ function resetCloud(input) {
 }
 
 function continueCloud(iValue) {
-  var leftMovementInterval = setInterval(function() {
-    var screenWidth = parseInt(window.innerWidth);
+  let leftMovementInterval = setInterval(function() {
+    let screenWidth = parseInt(window.innerWidth);
     currentCloudMargin[iValue] = currentCloudMargin[iValue] + (iValue + 1) / 10;
 
     if (currentCloud[iValue].style.marginLeft.slice(0, -2) > screenWidth) {
@@ -72,8 +72,8 @@ function continueCloud(iValue) {
 }
 biggestMoveLeft();
 //fish movement
-var currentFishMargin = [400, 800, 100, 600, 200, 1000, 300, 1200];
-var currentFish = [document.getElementsByClassName('small-fish')[0],
+let currentFishMargin = [400, 800, 100, 600, 200, 1000, 300, 1200];
+let currentFish = [document.getElementsByClassName('small-fish')[0],
   document.getElementsByClassName('small-fish')[1],
   document.getElementsByClassName('mid-fish')[0],
   document.getElementsByClassName('mid-fish')[1],
@@ -81,8 +81,8 @@ var currentFish = [document.getElementsByClassName('small-fish')[0],
   document.getElementsByClassName('big-fish')[1]
 ];
 
-var fishMovement = (function() {
-  for (var i = 0; i < currentFish.length; i++) {
+let fishMovement = (function() {
+  for (let i = 0; i < currentFish.length; i++) {
     if(i%2 === 0){
       continueFishRight(i);
     }
@@ -93,8 +93,8 @@ var fishMovement = (function() {
 });
 
 function continueFishLeft(i) {
-  var leftMovementInterval = setInterval(function() {
-    var screenWidth = parseInt(window.innerWidth);
+  let leftMovementInterval = setInterval(function() {
+    let screenWidth = parseInt(window.innerWidth);
     currentFishMargin[i] = currentFishMargin[i] - (i + 1)/4;
 
     if (currentFish[i].style.marginLeft.slice(0, -2) < -200) {
@@ -109,8 +109,8 @@ function continueFishLeft(i) {
 }
 
 function continueFishRight(i) {
-  var rightMovementInterval = setInterval(function() {
-    var screenWidth = parseInt(window.innerWidth);
+  let rightMovementInterval = setInterval(function() {
+    let screenWidth = parseInt(window.innerWidth);
     currentFishMargin[i] = currentFishMargin[i] + (i + 1)/4;
 
     if (currentFish[i].style.marginLeft.slice(0, -2) > screenWidth+200) {
